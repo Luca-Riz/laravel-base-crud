8e8511ec501ab14d8b667762113063ea1c8598da
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-10 15:55:17 +0200

    add redirect on ComicController

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -37,7 +37,23 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+        $data = $request->all();
+
+        $newComic = new Comic;
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+
+        $newComic->save();
+
+        //? una volta cliccato 'submit' sul form in 'create.blade.php' ci reindirizza in show con id del fumetto appena inserito
+        return redirect()->route('comics.show', $newComic->id);
+
     }
 
     /**
